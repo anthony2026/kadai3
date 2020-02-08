1996435c3f9bfff071f4bffb8945848a93aa542b
anthony2026 <sevenyears123456789@gmail.com>
2020-02-08 17:17:13 +0900

    Initial Commit Message

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,65 @@
+class BooksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :correct_user, only: [:edit, :update]
+
+  def index
+    @book = Book.new
+    @books = Book.all
+    @user = current_user
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have creatad book successfully."
+    redirect_to book_path(@book.id)
+  else
+    @user = current_user
+    @books = Book.all
+    render action: :index
+  end
+end
+
+  def show
+    @book = Book.find(params[:id])
+    @books = Book.all
+    @user = current_user
+  end
+
+  def new
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+  if @book.update(book_params)
+    flash[:notice] = "You have updated book successfully."
+    redirect_to book_path(@book)
+    else 
+      @user = current_user
+      render action: :show
+  end
+end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  def correct_user
+    @book = Book.find(params[:id])
+    if @book.user != current_user
+      redirect_to books_path
+    end
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '61fb729c09407de404b270c0c6678eaff20ce143f775393e7c4e3b3ea40ded19bd17fff9ab0e3ef4d64dfedea09e68bf834ab53614ede760236e34c79802622e'
